f65f4495684393b3648e63c637081f9026b982a5
Centny <centny@gmail.com>
2018-10-02 00:58:04 +0800

    remove package.

--- runner/install.sh
+++ runner/install.sh
@@ -1,2 +1,2 @@
-proxychains4 npm install git+https://github.com/sutils/crawler-pg.git --save
-npm install git+https://w.snows.io/news/crawler-news.git --save
+proxychains4 npm install git+https://github.com/sutils/crawler-pg.git
+npm install git+https://w.snows.io/news/crawler-news.git
